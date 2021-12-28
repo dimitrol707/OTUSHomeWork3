20e5938e5bd89f785a7a2510a9a2c1a6302f39d2
DIM <dimitrol7171@gmail.com>
2021-12-28 14:43:48 +0300

    Initial Commit

---
+++ OTUSHM3/OTUSHM3App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUSHM3App.swift
+//  OTUSHM3
+//
+//  Created by Дмитрий Мальцев on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUSHM3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
